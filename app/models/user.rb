98250164120a54cb790d50569240295dfad633db
kennedy <kenneyrit@gmail.com>
2023-03-26 02:26:33 +0300

    commented out the regex for the email

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, presence: true, format: { with: /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i, message: "must be a valid email address" }
+  validates :email, presence: true #format: { with: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, message: "must be a valid email address" }
 end
