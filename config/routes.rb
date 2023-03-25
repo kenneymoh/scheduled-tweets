4237f846a16edee09f9bbe0c0f0314b21686a49d
kennedy <kenneyrit@gmail.com>
2023-03-26 02:25:33 +0300

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
   get "about", to: "about#index"
 
   get "sign_up", to: "registrations#new"
-
   post "sign_up", to: "registrations#create"
 
   root to: "main#index"
