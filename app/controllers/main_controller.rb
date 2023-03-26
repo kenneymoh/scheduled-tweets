37ab61f81ddd06a0eca29b1c26d986be9cd9ca16
kennedy <kenneyrit@gmail.com>
2023-03-26 08:33:22 +0300

    updated the controllers

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,10 @@
 class MainController < ApplicationController
   def index
-    flash.now[:notice] = "Logged in successfuly"  ## stored in flash hash object
-    flash.now[:alert] = "Invalid email or passowrd"
+    # flash.now[:notice] = "Logged in successfuly"  ## stored in flash hash object
+    # flash.now[:alert] = "Invalid email or passowrd"
+
+    if session[:user_id]
+      @user = User.find_by(id: session[:user_id])
+    end
   end
 end
