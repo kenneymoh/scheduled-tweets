2d7f67b924ac48820240bbb11b6a5b75048b4890
kennedy <kenneyrit@gmail.com>
2023-03-27 07:49:36 +0300

    require user logged in method was defined in the application controller

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,8 +3,6 @@ class MainController < ApplicationController
     # flash.now[:notice] = "Logged in successfuly"  ## stored in flash hash object
     # flash.now[:alert] = "Invalid email or passowrd"
 
-    if session[:user_id]
-      @user = User.find_by(id: session[:user_id])
-    end
+    
   end
 end
