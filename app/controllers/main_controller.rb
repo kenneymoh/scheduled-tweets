99a647009b04468cfdeb3a184b7ffe132a37dd12
kennedy <kenneyrit@gmail.com>
2023-07-12 23:58:43 +0300

    added the flash notice and alert

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,8 @@
 class MainController < ApplicationController
   def index
-    # flash.now[:notice] = "Logged in successfuly"  ## stored in flash hash object
-    # flash.now[:alert] = "Invalid email or passowrd"
+    flash.now[:notice] = "Logged in successfuly"  ## stored in flash hash object
+    flash.now[:alert] = "Invalid email or passowrd"
 
-    
+
   end
 end
